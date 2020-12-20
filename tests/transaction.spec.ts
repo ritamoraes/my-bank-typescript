72733ea579ed5555fcec09cb5df4a960e5da336c
Rita Moraes <rmoraes@thoughtworks.com>
2020-12-20 00:16:00 -0300

    Lint adjusts

--- tests/transaction.spec.ts
+++ tests/transaction.spec.ts
@@ -1,18 +1,18 @@
-import { CheckingAccount } from "../src/domain/CheckingAccount";
+import { CheckingAccount } from '../src/domain/checking-account';
 
-describe("transaction with success", () => {
-  it("should return the correct amount of entries and debits", () => {
+describe('transaction with success', () => {
+  it('should return the correct amount of entries and debits', () => {
     const checkingAccount = new CheckingAccount();
-    checkingAccount.transaction("A", "B", 10);
-    checkingAccount.transaction("A", "B", 10);
+    checkingAccount.transaction('A', 'B', 10);
+    checkingAccount.transaction('A', 'B', 10);
 
     const {
       accountId,
       totalDebit,
-      totalCredit,
-    } = checkingAccount.getTransaction("A");
+      totalCredit
+    } = checkingAccount.transactionSummary('A');
 
-    expect(accountId).toEqual("A");
+    expect(accountId).toEqual('A');
     expect(totalCredit).toEqual(0);
     expect(totalDebit).toEqual(20);
   });
