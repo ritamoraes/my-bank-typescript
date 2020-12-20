3f10c32d1d083a5ee56fd42bcbcd7266ddf49af9
Rita Moraes <rmoraes@thoughtworks.com>
2020-12-20 01:14:17 -0300

    Installs inversifyJs to dependency injection

--- tests/transaction.spec.ts
+++ tests/transaction.spec.ts
@@ -1,16 +1,28 @@
 import { CheckingAccount } from '../src/domain/checking-account';
+import {Container} from "inversify";
+import {CheckingAccountService} from "../src/checking-account-service";
 
 describe('transaction with success', () => {
+
+  let checkingAccountMain: CheckingAccount;
+
+  beforeEach(()=>{
+    const container = new Container();
+    container.bind<CheckingAccountService>(CheckingAccountService).to(CheckingAccountService);
+    checkingAccountMain = container.resolve(CheckingAccount);
+
+  })
+
   it('should return the correct amount of entries and debits', () => {
-    const checkingAccount = new CheckingAccount();
-    checkingAccount.transaction('A', 'B', 10);
-    checkingAccount.transaction('A', 'B', 10);
+
+    checkingAccountMain.transaction('A', 'B', 10);
+    checkingAccountMain.transaction('A', 'B', 10);
 
     const {
       accountId,
       totalDebit,
       totalCredit
-    } = checkingAccount.transactionSummary('A');
+    } = checkingAccountMain.transactionSummary('A');
 
     expect(accountId).toEqual('A');
     expect(totalCredit).toEqual(0);
